af25a43c2dd1529ea968c83dc9508d08d1e34247
zsu13579 <jacklvabcd@163.com>
2017-07-02 11:56:56 +0800

    add game of life

--- src/routes/roguelike/Roguelike.js
+++ src/routes/roguelike/Roguelike.js
@@ -6,101 +6,105 @@ import s from './Roguelike.css';
 import { connect } from 'react-redux';
 import { setRuntimeVariable } from '../../actions/runtime';
 
-var Btn=React.createClass({
-  handleSize:function(){
-    var row=this.props.row;
-    var col=this.props.col;
-    this.props.handleSize(row,col);
-  },
-  render: function(){
-    return <button onClick={this.handleSize}>{this.props.name}</button>
-  }
-});
+class Btn extends React.Component{
 
-var Ge=React.createClass({
-  getInitialState: function(){
-    var stage=this.props.stage;
-    return {stage:stage}
-  },
-  componentWillReceiveProps: function(nextProps){
+  constructor(...args) {
+    super(...args);
+  };
+
+  handleSize = function(){
+    let row=this.props.row;
+    let col=this.props.col;
+    this.props.handleSize(row,col);
+  };
+
+  render(){
+    return <button onClick={this.handleSize.bind(this)}>{this.props.name}</button>
+  }
+};
+
+class Ge extends React.Component{
+
+  constructor(...args){
+    super(...args);
+    let stage = this.props.stage;
+    this.state = {stage:stage}
+  };
+
+  componentWillReceiveProps = function(nextProps){
     this.setState({stage:nextProps.stage})
-  },
-  handleClick:function(){
-    var stage=this.state.stage;
-     var id="r"+this.props.row+"c"+this.props.col;
+  };
+  handleClick = function(){
+    let stage=this.state.stage;
+    let id="r"+this.props.row+"c"+this.props.col;
     if(stage=="empty"){
       stage="newborn";
       this.setState({stage:stage});
       this.props.handleChange(id,stage);
     }
-  },
-  render: function(){
-    var id="r"+this.props.row+"c"+this.props.col;
+  };
+  render(){
+    let id="r"+this.props.row+"c"+this.props.col;
     return (
-      <span id={id} className={this.state.stage} onClick={this.handleClick}></span>
+      <span id={id} className={s[this.state.stage]} onClick={this.handleClick.bind(this)}></span>
       )
-  }
-});
-
-var SetIntervalMixin = {
-  componentWillMount: function() {
-    this.intervals = [];
-  },
-  setInterval: function() {
-    this.intervals.push(setInterval.apply(null, arguments));
-  },
-  componentWillUnmount: function() {
-    this.intervals.forEach(clearInterval);
-  }
+  };
 };
 
-var Roguelike=React.createClass({ 
-  mixins: [SetIntervalMixin], 
-  getInitialState:  function(){
-    var board={};
-    var row=50;
-    var col=70;
-    var gen=0;
-    var choice=['empty','newborn','adult'];
-      for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
-      var id="r"+i+"c"+j;
-      var vr=Math.floor(Math.random()*3);
+class Roguelike extends React.Component{ 
+  constructor(...args){
+    super(...args);
+    let board={};
+    let row=50;
+    let col=70;
+    let gen=0;
+    let choice=['empty','newborn','adult'];
+      for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
+      let id="r"+i+"c"+j;
+      let vr=Math.floor(Math.random()*3);
       board[id]=choice[vr];
       };
     };
-   // board.r2c2="adult"; 
-   // board.r2c1="adult";
-   // board.r2c3="adult";
-    return {row:row,col:col,board:board,isPause:0,isClear:0,gen:gen,speed:300}
-  },
-  handleSize:function(row,col){
-    var board={};
-    for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
-      var id="r"+i+"c"+j;
+
+    this.state = {row:row,col:col,board:board,isPause:0,isClear:0,gen:gen,speed:300}
+
+  };
+
+  setInterval = function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  };
+
+  handleSize = function(row,col){
+    let board={};
+    for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
+      let id="r"+i+"c"+j;
       board[id]="empty";
       };
     };
-  this.setState({row:row,col:col,board:board});  
-  },
-  handleSpeed:function(e){
-  var speed=e.target.id;
-  speed=parseInt(speed);
-  this.setState({speed:speed});
-  this.handlePause();     this.timer=this.setInterval(this.generate,speed);
-  },
-  handleChange:function(id,stage){
-    var board=this.state.board;
+    this.setState({row:row,col:col,board:board});  
+  };
+
+  handleSpeed = function(e){
+    let speed=e.target.id;
+    speed=parseInt(speed);
+    this.setState({speed:speed});
+    this.handlePause();     
+    this.timer=this.setInterval(this.generate.bind(this),speed);
+  };
+
+  handleChange = function(id,stage){
+    let board=this.state.board;
     board[id]=stage;
     this.setState({board:board});
-  },
-  findLiveNeighbours:function(i,j){
-    var col=this.state.col;
-    var row=this.state.row;
-    var board=this.state.board;
-    var id="";
-    var dcounter=0;
+  };
+  findLiveNeighbours = function(i,j){
+    let col=this.state.col;
+    let row=this.state.row;
+    let board=this.state.board;
+    let id="";
+    let dcounter=0;
         if(board["r"+(i-1)+"c"+(j-1)]=="adult"){
           dcounter++
         };
@@ -126,23 +130,23 @@ var Roguelike=React.createClass({
           dcounter++
         };
     return dcounter;
-  },
-  generate:function(){
-    var col=this.state.col;
-    var row=this.state.row;
-    var board=this.state.board;
-    var id="";
+  };
+  generate = function(){
+    let col=this.state.col;
+    let row=this.state.row;
+    let board=this.state.board;
+    let id="";
     // for newborn
-    var counter=0;
+    let counter=0;
     // for  die
-    var dcounter=0;
-    var dieIds=[];
-    var newbornIds=[];
-    var gen=this.state.gen;
+    let dcounter=0;
+    let dieIds=[];
+    let newbornIds=[];
+    let gen=this.state.gen;
     
     // set  all  newborn  to  adult
-    for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
+    for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
         id="r"+i+"c"+j;    
         if(board[id]=="newborn"){
           board[id]="adult";
@@ -151,8 +155,8 @@ var Roguelike=React.createClass({
     };  
     //find  newborn
     newbornIds=[];
-    for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
+    for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
         counter=0;
         id="r"+i+"c"+j;
         counter=this.findLiveNeighbours(i,j);
@@ -162,8 +166,8 @@ var Roguelike=React.createClass({
         };
     };    
     // find die box fewer than 2 neighbours;
-    for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
+    for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
         id="r"+i+"c"+j;    
         dcounter=this.findLiveNeighbours(i,j);
         if(board[id]=="adult" && dcounter<2){
@@ -172,8 +176,8 @@ var Roguelike=React.createClass({
       };
     }
     // find die box more than 3 neighbours;
-    for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
+    for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
         id="r"+i+"c"+j;    
         dcounter=this.findLiveNeighbours(i,j);
         if(board[id]=="adult" && dcounter>3){
@@ -181,88 +185,102 @@ var Roguelike=React.createClass({
         }
       };
     };
-        //set  dieIds  to  empty
-        if(dieIds!=[]){
-          dieIds.forEach(function(value){
-            board[value]="empty";
-          })
-        }
-        //set newborn id  to  newborn
-        if(newbornIds!=[]){
+    //set  dieIds  to  empty
+    if(dieIds!=[]){
+      dieIds.forEach(function(value){
+        board[value]="empty";
+      })
+    }
+    //set newborn id  to  newborn
+    if(newbornIds!=[]){
       newbornIds.forEach(function(value){
-        board[value]="newborn";
-         })
-        }
+      board[value]="newborn";
+     })
+    }
     gen++;
-  this.setState({row:row,col:col,board:board,gen:gen});
-  },
-  componentWillMount:function(){
+    this.setState({row:row,col:col,board:board,gen:gen});
+  };
+  componentDidMount = function(){
+    this.intervals = [];
     this.handleStart();
-  },
-  handleClear:function(){
-    var row=this.state.row;
-    var col=this.state.col;
-      for(var i=0;i<row;i++){
-      for(var j=0;j<col;j++){
-      var id="r"+i+"c"+j;
+  };
+  componentWillUnmount = function(){
+    if(this.intervals){
+      this.intervals.forEach(clearInterval);
+    }
+  };
+
+  handleClear = function(){
+    let row=this.state.row;
+    let col=this.state.col;
+    let board={};
+    for(let i=0;i<row;i++){
+      for(let j=0;j<col;j++){
+      let id="r"+i+"c"+j;
       board[id]="empty";
       };
     };
-  // this.intervals.forEach(clearInterval);
-  this.componentWillUnmount();
-  this.setState({board:board,isClear:1,gen:0});
-  },
-  handlePause:function(){    
-  // this.intervals.forEach(clearInterval);
-  this.componentWillUnmount();
-  },
-  handleStart:function(){
-    var isPause=this.state.isPause;
-    var isClear=this.state.isClear;
-    var speed=this.state.speed;
+    this.intervals.forEach(clearInterval);
+    // this.componentWillUnmount();
+    this.setState({board:board,isClear:1,gen:0});
+  };
+
+  handlePause = function(){    
+    this.intervals.forEach(clearInterval);
+    // this.componentWillUnmount();
+  };
+
+  handleStart = function(){
+    let isPause=this.state.isPause;
+    let isClear=this.state.isClear;
+    let speed=this.state.speed;
     
     isPause=0;
     isClear=0;
     // this.setState({isPause:0,isClear:0});
     // if(isPause==0 && isClear==0)
-    this.timer=this.setInterval(this.generate,speed);
-},
+    this.timer=this.setInterval(this.generate.bind(this),speed);
+  };
   
-  render: function(){
-    var geList=[];
-    var col=this.state.col;
-    var row=this.state.row;
-    var board=this.state.board;
-    var id="";
-    var stage="";
-    for(var i=0;i<row;i++){
-      var rowList=[];
-      for(var j=0;j<col;j++){     
+  render(){
+    let geList=[];
+    let col=this.state.col;
+    let row=this.state.row;
+    let board=this.state.board;
+    let id="";
+    let stage="";
+    for(let i=0;i<row;i++){
+      let rowList=[];
+      for(let j=0;j<col;j++){     
       id="r"+i+"c"+j;
       stage=board[id];    
-      rowList.push(<Ge row={i} col={j} stage={stage} handleChange={this.handleChange} />);
+      rowList.push(<Ge row={i} col={j} stage={stage} handleChange={this.handleChange.bind(this)} />);
       };
-      geList.push(<tr>{rowList}</tr>);
+      geList.push(<tr><td>{rowList}</td></tr>);
     };
     return (
-      <div id="board">
-        <div className="topBar">
-        <button onClick={this.handleStart}>Start</button>
-          <button onClick={this.handlePause}>Pause</button>
-        <button onClick={this.handleClear}>Clear</button>
+      <div id={s.board}>
+        <div className={s.topBar}>
+        <button onClick={this.handleStart.bind(this)}>Start</button>
+        <button onClick={this.handlePause.bind(this)}>Pause</button>
+        <button onClick={this.handleClear.bind(this)}>Clear</button>
         <div className="gen">Generation:{this.state.gen}</div>
-        </div> 
-        {geList}
-        <Btn handleSize={this.handleSize} name="Size:50x30" row="30" col="50" />
-        <Btn handleSize={this.handleSize} name="Size:70x50" row="50" col="70" />
-         <Btn handleSize={this.handleSize} name="Size:100x80" row="80" col="100" />
-        <button onClick={this.handleSpeed} id="1500">Slow</button>
-        <button onClick={this.handleSpeed} id="800">Medium</button>
-        <button onClick={this.handleSpeed} id="300">Fast</button>
+        </div>
+        <table> 
+          <tbody>
+            {geList}
+          </tbody>
+        </table>
+        <Btn handleSize={this.handleSize.bind(this)} name="Size:50x30" row="30" col="50" />
+        <Btn handleSize={this.handleSize.bind(this)} name="Size:70x50" row="50" col="70" />
+        <Btn handleSize={this.handleSize.bind(this)} name="Size:100x80" row="80" col="100" />
+        <button onClick={this.handleSpeed.bind(this)} id="1500">Slow</button>
+        <button onClick={this.handleSpeed.bind(this)} id="800">Medium</button>
+        <button onClick={this.handleSpeed.bind(this)} id="300">Fast</button>
       </div>
     )
   }
-});
+};
 
 
 // class Roguelike extends React.Component {
